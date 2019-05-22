425de527ab0a283bfcd16058c36bb05427bbf948
Samantha Kane <s.kane133@gmail.com>
2019-05-22 20:55:35 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+//var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name){
+    katzDeliLine.push(name)
+  return ("Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.")
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  }
+  else{
+    var name = katzDeliLine[0]
+    katzDeliLine.shift()
+    return "Currently serving " + name + ".";
+  }
+}
+      function currentLine(line){
+     var long = [ ];
+     if (line.length === 0){
+       return "The line is currently empty."
+     }
+     else { 
+       for (var i = 0; i < line.length; i++){
+       long.push( " " + (i+ 1) + ". " + line[i]);
+     }
+     
+       return "The line is currently:" + long; 
+   }
+ }
